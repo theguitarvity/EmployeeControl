1679aa8a849637c69b5f5044d0fb2668e759f8d4
Victor Lopes <contato_victorlopes@hotmail.com>
2018-05-16 18:07:02 -0400

    Adicionando Classes dda model

---
+++ app/src/main/java/Office.java
@@ -0,0 +1,10 @@
+public class Office {
+    private long codeOffice;
+    private String nameOffice;
+
+    public Office(long codeOffice, String nameOffice) {
+        this.codeOffice = codeOffice;
+        this.nameOffice = nameOffice;
+    }
+    
+}
